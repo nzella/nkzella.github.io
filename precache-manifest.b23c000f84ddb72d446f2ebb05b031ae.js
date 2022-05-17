47a7d456b2d8603229657d7f0055c5b30382388e
Natasha Zella <natashazella@Natashas-MacBook-Pro.local>
2022-05-17 12:43:48 -0700

    Updates

---
+++ precache-manifest.b23c000f84ddb72d446f2ebb05b031ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0a2b7fd0245e1feff8a",
+    "url": "/static/css/main.8672d4e9.chunk.css"
+  },
+  {
+    "revision": "e0a2b7fd0245e1feff8a",
+    "url": "/static/js/main.c0fd6114.chunk.js"
+  },
+  {
+    "revision": "bb1f08fe37d3c65590eb",
+    "url": "/static/js/runtime~main.e015284a.js"
+  },
+  {
+    "revision": "ef72a72af5b5b1ac0e44",
+    "url": "/static/js/2.b6a0e90a.chunk.js"
+  },
+  {
+    "revision": "a03a2b79c7dfa4b66d3d",
+    "url": "/static/js/3.7fa365a8.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "b96bf0e7f7db398f35b925154fd9724f",
+    "url": "/index.html"
+  }
+];
